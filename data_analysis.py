53849ab015f3a785aa5b836b195056b5bf483c18
Ginika Elizabeth Nna <elizabethginika9@gmail.com>
2024-05-06 02:52:44 +0100

    data_manipulation

--- data_analysis.py
+++ data_analysis.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 
 #filling in the path to my working directory
 file_path = ""
@@ -18,3 +19,36 @@ plt.xlabel('X label')
 plt.ylabel('Y label')
 plt.title('Title')
 plt.show()
+
+# Convert date column to date format
+transaction.data['DATE'] = pd.to_datetime(transaction.data['DATE'], origin='1899-12-30')
+
+# Generate the summary of a product name column
+product_name_summary = transaction_data['PROD_NAME'].value_counts()
+print(product_name_summary)
+
+# split product name into individual words
+individual_words = transaction_data['PROD_NAME'].str.split(expand=True).stack().unique()
+
+# Removing digits and special characters
+individual_words = individual_words.str.replace(r'[^a-zA-Z]', '')
+
+# Removing salsa products
+transaction_data = transaction_data[~transaction_data['PROD_NAME'].str.contains('salsa', case=False)]
+
+# counting frequency for each word
+word_frequency = individual_words.value_counts()
+print(word_frequency)
+
+#check summary statistics and for null
+print(transaction_data.describe())
+print(transaction_data.isnull().sum())
+
+# investigate outliers - transaction with large quantity of chips
+product_outliers = transaction_data[transaction_data['PROD_QTY'] == 200]
+
+# investigating other transaction by the same customer
+customer_transaction = transaction_data[transaction_data['LYLTY_CARD_NBR'].isin(product_outlier['LYLTY_CARD_NBR'])]
+print(customer_transaction)
+
+
